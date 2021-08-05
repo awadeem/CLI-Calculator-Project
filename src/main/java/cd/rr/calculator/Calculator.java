4bdec22c6181b1469773ca957382b1b766f19340
Michiah <85957194+Michiah@users.noreply.github.com>
2021-08-05 11:12:58 -0400

    update

--- src/main/java/cd/rr/calculator/Calculator.java
+++ src/main/java/cd/rr/calculator/Calculator.java
@@ -22,8 +22,7 @@ public class Calculator {
         System.out.println("Second Number: ");
         double secondNumber = input.nextDouble();
 
-        System.out.println("Michiah was here");
-        System.out.println("here again");
+
 
 
     }
